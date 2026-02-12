975c3f8b6949b28efd1736ca25a891359f2a7741
Yaghs <syaghinloo@gmail.com>
2026-02-12 16:24:18 -0500

    added additional styling to the about me page and the header, replaced the old resume with an updated one

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About(){
                 <div className="flex-1 text-center lg:text-left">
                 <h1 className="text-4xl lg:text-5xl mb-6 lg:mb-10">About Me</h1>
                 <p>I am a recent Computer Science graduate from SUNY Farmingdale, having
-                earned my Bachelor's degree on May 21, 2025, with a cumulative GPA of
+                earned my Bachelor&apos;s degree on May 21, 2025, with a cumulative GPA of
                 3.4. I gained professional experience as a Data Scientist Intern with
                 the U.S. Food and Drug Administration during Summer 2024 and Spring
                 2025. I am a dedicated and motivated individual, eager to apply my
@@ -34,7 +34,7 @@ export default function About(){
                                     hover:bg-blue-50
                                     transition duration-300"
                         href="https://www.linkedin.com/in/shahin-yaghinloo-00380a1a7/">
-                         <div className="flex items-center justify-center gap-3">
+                         <div className="flex items-center justify-center gap-3 pl-5 lg:pl-0">
                         <Image src="/linkedinlogo.png" alt="linkedIn-logo" width={50} height={50} />
                         LinkedIn
                         </div>
@@ -46,9 +46,9 @@ export default function About(){
                                     hover:bg-blue-50
                                     transition duration-300"
                         href="mailto:syaghinloo@gmail.com">
-                         <div className="flex items-center justify-center gap-3">
-                        <Image src="/Gmailogo.png" alt="Gmail-logo" width={50} height={50}/>
-                        Gmail
+                         <div className="flex items-center justify-center gap-3 pt-1.5">
+                            <Image src="/Gmailogo.png" alt="Gmail-logo" width={50} height={50}/>
+                            Gmail
                         </div>
                     </Link>
                 </div>
@@ -58,52 +58,52 @@ export default function About(){
                 className="rounded-[10px] shadow-lg object-cover shrink-0"/>
             </div>
                 <div>
-                    <h1 className="text-3xl pt-20">Programming languages</h1>
+                    <h1 className="text-3xl pt-20 font-bold">Programming languages</h1>
                     <div className="flex justify-center gap-10 pt-10">
                         <div className="relative inline-block group">
                             <Image  src="/JavaScript.png" alt="Java-script-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">JavaScript</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image  src="/HTML.png" alt="html-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">HTML</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/CSS.png" alt="Java-script-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">CSS</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
-                            <Image  src="/TypeScript.png" alt="Type-script-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <Image  src="/TypeScript.png" alt="Type-script-logo" width={200} height={200} className="rounded-4xl"/>
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Typscript</span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <h1 className="text-3xl pt-20">Frameworks/Libraries</h1>
+                    <h1 className="text-3xl pt-20 font-bold">Frameworks/Libraries</h1>
                      <div className="flex justify-center gap-10 pt-10">
                         <div className="relative inline-block group">
                             <Image src="/React.png" alt="React-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">React</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/NextJs.png" alt="Next-js-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Next JS</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/tailwindCSS.png" alt="tailwind-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Tailwind CSS</span>
                             </div>
                         </div>
@@ -112,29 +112,29 @@ export default function About(){
                 </div>
 
                 <div>
-                    <h1 className="text-3xl pt-20">Technologies</h1>
+                    <h1 className="text-3xl pt-20 font-bold">Technologies</h1>
                      <div className="flex justify-center gap-10 pt-10">
                         <div className="relative inline-block group">
                             <Image src="/Firebase-new.png" alt="Firebase-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Firebase</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/github.png" alt="Next-js-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Git</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/claude-color.svg" alt="claude-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Claude</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/OpenAI.png" alt="claude-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">OpenAI API</span>
                             </div>
                         </div>
@@ -143,17 +143,17 @@ export default function About(){
                 </div>
 
                 <div>
-                    <h1 className="text-3xl pt-20">Tools</h1>
+                    <h1 className="text-3xl pt-20 font-bold">Tools</h1>
                      <div className="flex justify-center gap-10 pt-10">
                         <div className="relative inline-block group">
                             <Image src="/VS-Code.png" alt="VS-Code-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">VS-Code</span>
                             </div>
                         </div>
                         <div className="relative inline-block group">
                             <Image src="/Cursor.png" alt="Cursor-logo" width={200} height={200}/>
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 transition duration-300">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:bg-black hover:opacity-90 rounded-4xl transition duration-300">
                                 <span className="text-white text-xl font-semibold">Cursor</span>
                             </div>
                         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
-          <nav className="flex flex-wrap justify-center items-center bg-white h-16 shadow-xl p-4 gap-6 relative z-2">
+          <nav className="sticky top-0 flex flex-wrap justify-center items-center bg-white h-16 shadow-xl p-4 gap-6 w-full z-10">
             <Link className=" text-black text-sm hover:text-blue-500 transition duration-300" href="/">Home</Link>
             <Link className=" text-black text-sm hover:text-blue-500 transition duration-300" href="/about">About</Link>
-            <Link className=" text-black text-sm hover:text-blue-500 transition duration-300" href="/Shahin_Yaghinloo_Resume_V14.pdf" download="Shahin-Yaghinloo-Resume">Resume</Link>
+            <Link className=" text-black text-sm hover:text-blue-500 transition duration-300" href="/Shahin_Yaghinloo_Resume_V16.pdf" download="Shahin-Yaghinloo-Resume">Resume</Link>
           </nav>
         {children}
       </body>
